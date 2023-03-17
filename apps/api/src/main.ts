8ab8a30f41074a677f4cda8a632988d30c40dd1f
alaa <alaa.mady@coformatique.com>
2023-03-17 16:07:07 +0200

    remove frontend from deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,12 @@ import * as express from 'express';
 import * as path from 'path';
 import app from './app';
 
-const CLIENT_BUILD_PATH = path.join(
-  __dirname,
-  '../../../../frontend/index.html'
-);
+// const CLIENT_BUILD_PATH = path.join(
+//   __dirname,
+//   '../../../../frontend/index.html'
+// );
 
-app.use(express.static(CLIENT_BUILD_PATH));
+// app.use(express.static(CLIENT_BUILD_PATH));
 
 const greeting = { message: 'Welcome to api!' };
 
@@ -15,13 +15,12 @@ app.get('/api', (req, res) => {
   res.send(greeting);
 });
 
-app.get('*', (request, response) => {
-  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
-});
+// app.get('*', (request, response) => {
+//   response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+// });
 
-const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const server = app.listen(port, () => {
-  console.log('Listening at ' + host + port + '/api');
+  console.log('Listening at port' + port + '/api');
 });
 server.on('error', console.error);

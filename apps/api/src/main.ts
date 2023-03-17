33fa77c5382c5d9fd6c9a543d351933b78dc97e6
alaa <alaa.mady@coformatique.com>
2023-03-17 15:11:50 +0200

    setup build and start scripts and Pocfile for heroku

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,27 @@
+import * as express from 'express';
+import * as path from 'path';
 import app from './app';
+
+const CLIENT_BUILD_PATH = path.join(
+  __dirname,
+  '../../../../frontend/index.html'
+);
+
+app.use(express.static(CLIENT_BUILD_PATH));
+
+const greeting = { message: 'Welcome to api!' };
+
+app.get('/api', (req, res) => {
+  res.send(greeting);
+});
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+});
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+const server = app.listen(port, () => {
+  console.log('Listening at ' + host + port + '/api');
 });
+server.on('error', console.error);
